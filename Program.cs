e2521aab28ded4240fd6fd545923c1dbba7616cc
Cristian Iacoboaei <cristian.iacoboaei@autototal.ro>
2016-05-09 11:02:40 +0300

    Second @

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace GitSourceControlProvider
         static void Main(string[] args)
         {
             Console.WriteLine("Test");
+            Console.WriteLine("Second!");
         }
     }
 }
